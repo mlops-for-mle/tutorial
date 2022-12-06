e146d9bd1a9f837b3d671b2096118286f74bd5fc
Aiden-Jeon <aiden.jongseob@gmail.com>
2022-12-06 18:11:45 +0900

    add sitemap plugin
    Signed-off-by: Aiden-Jeon <aiden.jongseob@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,12 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
       }),
     ],
   ],
